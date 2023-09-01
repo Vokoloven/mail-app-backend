15caabbbdbcccea545e6df2599266a44f28f5c9b
Ruslan Volovenko <vokoloven@gmail.com>
2023-09-01 14:29:17 +0300

    feat: added validation

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,8 +1,37 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+enum Role {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+enum State {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
 export class UsersDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsEnum(Role)
+  @IsNotEmpty()
   role: 'admin' | 'user';
+
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
+
+  @IsEnum(State)
+  @IsNotEmpty()
   state: 'male' | 'female';
 }
